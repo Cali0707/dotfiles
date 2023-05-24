0d6608828bb1e6b075c558f3a8da8c10a2c6fcbb
Calum Murray <cmurray@redhat.com>
2023-05-24 13:35:42 -0400

    Started using lazy.nvim

--- nvim/.config/nvim/lua/user/autocommands.lua
+++
@@ -1,8 +0,0 @@
-local api = vim.api
-
--- automatically highlight text on yank
-local yankGroup = api.nvim_create_augroup("YankHighlight", { clear = true })
-api.nvim_create_autocmd("TextYankPost", {
-    command = "silent! lua vim.highlight.on_yank()",
-    group = yankGroup,
-})
