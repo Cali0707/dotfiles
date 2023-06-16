3d055b042c76ee85ba0dcaf9b9fe032c1fdd7526
Calum Murray <cmurray@redhat.com>
2023-06-16 09:42:42 -0400

    Removed nvim-cmp keymaps

--- nvim/lua/base/coding.lua
+++ nvim/lua/base/coding.lua
@@ -64,50 +64,7 @@ return {
 						require("luasnip").lsp_expand(args.body)
 					end,
 				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping({
-						i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
-						c = function(fallback)
-							if cmp.visible() then
-								cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-							else
-								fallback()
-							end
-						end,
-					}),
-					["<C-j>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jump()
-						elseif has_words_before() then
-							cmp.complete()
-						else
-							fallback()
-						end
-					end, {
-						"i",
-						"s,",
-						"c",
-					}),
-					["<C-k>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, {
-						"i",
-						"s,",
-						"c",
-					}),
-				}),
 				sources = cmp.config.sources({
-					{ name = "nvim_lsp_signature_help", group_index = 1 },
 					{ name = "nvim_lsp", group_index = 1 },
 					{ name = "luasnip", group_index = 1 },
 					{ name = "buffer", group_index = 2 },
